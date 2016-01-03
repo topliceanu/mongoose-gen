f477aab122b91a5f2d7c1e9e6681862cdcbfafd3
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2016-01-03 14:28:54 +0000

    fix the bagrant bash provisioning script

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-## Install mongodb.
-#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-#echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-#sudo apt-get update
-#sudo apt-get install -y mongodb-org-server mongodb-org-shell
-#
+# Install mongodb.
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org-server mongodb-org-shell
+
 # Install nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
 echo ". ~/.nvm/nvm.sh" >> ~/.bash_profile
